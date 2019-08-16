be401e862f5d87c3e28a514eb23166aaeeb6ad8c
fauziarda <fauziardasaputra@gmail.com>
2019-08-16 20:11:25 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// ///////////////////
+//  starting ponts of app
+// /////////////////////
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/',function(request,response) {
+    response.render('index.ejs');
+});
+// dealing with socket.io
+io.sockets.on('connection',function(socket) {
+    // ketika ada user yang masuk / join ke chat
+    socket.on('username',function(username) {
+        socket.username = username;
+        io.emit('is_online','🔵<i>' + socket.username + ' join the chat.. </i>');
+    })
+    // ketika ada user yang left
+    socket.on('disconnect',function(username) {
+        io.emit('is_online','🔴 <i>' + socket.username + ' left the chat.. </i>');
+    });
+    // ketika salah satu user mengirimkan pesan
+    socket.on('chat_message',function(message) {
+        io.emit('chat_message','<strong>' + socket.username + '</strong> : ' + message);
+    });
+});
+const server = http.listen(8000,function() {
+    console.log('listening on *:8000');
+});
